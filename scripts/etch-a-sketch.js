56dc59878d5769949824dda3e563cac61050bb87
Kimberley Chan <cmaylynn12@gmail.com>
2021-06-16 09:12:19 +1000

    Added clear and reset grid button

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -13,25 +13,43 @@ function setUpDrawingPad(columns, rows) {
         let gridBox = document.createElement('div');
         gridBox.id = i;
         drawPad.appendChild(gridBox).className = "grid-box";
-        
     }
-}
-
-function start() {
     
-    setUpDrawingPad(100, 100);
-
     function colorTheGrid(e) {
         e.target.classList.add('color-box');
-        
     }
 
     let individualGridBox = document.querySelectorAll('.grid-box');
-    console.log(individualGridBox);
 
     individualGridBox.forEach((gridBox) => {
         gridBox.addEventListener('mouseover', colorTheGrid);
     })
+}
+
+function start() {
+    
+    setUpDrawingPad(16, 16);
+
+
+    function clearTheGrid() {
+
+        let drawPad = document.querySelector('#draw-pad');
+        let individualGridBox = document.querySelectorAll('.grid-box');
+
+        individualGridBox.forEach((gridBox) => {
+            drawPad.removeChild(gridBox);
+
+        })
+        
+        let cols = prompt('Enter new grid: ');
+        let rows = cols;
+
+        setUpDrawingPad(cols, rows);
+    }
+
+    const clearButton = document.querySelector("#clear-btn");
+    clearButton.addEventListener('click', clearTheGrid);
+
 }
 
 start();
